524cce31d5ae5f43dc6cd4dd434f3a2ea56898b0
Mary Katherine McKenzie <mkmckenzie91@gmail.com>
2016-02-12 17:37:15 -0500

    first commit

--- app/models/nasa_apod_api.rb
+++ app/models/nasa_apod_api.rb
@@ -1,2 +1,11 @@
 class NasaApodApi
+
+  def self.get_nasa_info
+    nasa = URI("https://api.nasa.gov/planetary/apod?api_key=#{ENV['NASA_API_KEY']}")
+    nasa_info = Net::HTTP.get(nasa)
+    JSON.parse(nasa_info)
+  end
+
+
+
 end
