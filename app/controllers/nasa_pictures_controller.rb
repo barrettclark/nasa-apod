8bf15d8059f4d8727182ea4e2edb5ab5beb787de
Mary Katherine McKenzie <mkmckenzie91@gmail.com>
2016-02-13 17:15:20 -0500

    Add picture/video + title to view

--- app/controllers/nasa_pictures_controller.rb
+++ app/controllers/nasa_pictures_controller.rb
@@ -1,4 +1,6 @@
 class NasaPicturesController < ApplicationController
   def index
+  	@nasa_info = NasaApodApi.get_nasa_info
+  	@url_type_is_youtube = @nasa_info["url"].include?('youtube.com')
   end
 end
