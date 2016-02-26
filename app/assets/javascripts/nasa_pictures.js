74e63997b31e4465d9170f89acad96c0421569e2
Mary Katherine McKenzie <mkmckenzie91@gmail.com>
2016-02-26 15:24:04 -0500

    Update jquery

--- app/assets/javascripts/nasa_pictures.js
+++ app/assets/javascripts/nasa_pictures.js
@@ -1,24 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(function () {
-    // cache the sliding object in a var
-    var slideout = $('#summary');
-    // click-me not clickme
-    $("#clickToOpen").toggle(function () {
-        // use cached object instead of searching
-        slideout.animate({
-            right: '0px'
-        }, {
-            queue: false,
-            duration: 500
-        });
-    }, function () {
-        // use cached object instead of searching        
-        slideout.animate({
-            right: '-300px'
-        }, {
-            queue: false,
-            duration: 500
-        });
-    });
-});
+$(document).ready(function() {
+	$("#clickToOpen").click(function() {
+		$("#summary").animate({right:'200px'});
+	});
+});      
