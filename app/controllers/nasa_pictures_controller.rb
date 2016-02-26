d8787ed55cab34ebca5fccab71c357e7e95b16e1
Mary Katherine McKenzie <mkmckenzie91@gmail.com>
2016-02-26 17:09:59 -0500

    add caching in memory

--- app/controllers/nasa_pictures_controller.rb
+++ app/controllers/nasa_pictures_controller.rb
@@ -1,4 +1,5 @@
 class NasaPicturesController < ApplicationController
+  
   def index
   	@nasa_info = NasaApodApi.get_nasa_info(params[:date])
   	@media_type_is_video = @nasa_info["media_type"].eql?("video")
--- app/models/nasa_apod_api.rb
+++ app/models/nasa_apod_api.rb
@@ -1,9 +1,16 @@
 class NasaApodApi
 
+  @@dates = {}
+
   def self.get_nasa_info(set_date=Date.today.to_s)
-    nasa = URI("https://api.nasa.gov/planetary/apod?api_key=#{ENV['NASA_API_KEY']}&date=#{set_date}")
-    nasa_info = Net::HTTP.get(nasa)
-    JSON.parse(nasa_info)
+    unless @@dates.include?(set_date)
+      Rails.logger.debug "fetching #{set_date}"
+      nasa = URI("https://api.nasa.gov/planetary/apod?api_key=#{ENV['NASA_API_KEY']}&date=#{set_date}")
+      nasa_info = Net::HTTP.get(nasa)
+      @@dates[set_date] = JSON.parse(nasa_info)
+    end
+    @@dates[set_date]
+
   end
 
 end
