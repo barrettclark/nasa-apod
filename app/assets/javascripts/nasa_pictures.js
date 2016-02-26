5f0781abca2f09f4251b82f05eaef7a6d8e7e27a
Mary Katherine McKenzie <mkmckenzie91@gmail.com>
2016-02-26 16:17:52 -0500

    Add jquery and fixed route

--- app/assets/javascripts/nasa_pictures.js
+++ app/assets/javascripts/nasa_pictures.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function() {
-	$("#clickToOpen").click(function() {
-		$("#summary").animate({right:'200px'});
+	$("div#clickToOpen").on("click", function() {
+		$("div#summaryAndDate").toggle("slow");
 	});
-});      
+});    
