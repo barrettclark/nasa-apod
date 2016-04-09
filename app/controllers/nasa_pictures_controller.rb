4392cb81fa4c6fe40ab91124df79cd806a36c575
Mary Katherine McKenzie <mkmckenzie91@gmail.com>
2016-04-09 16:47:39 -0400

    fix controller to not default to null if no date param supplied

--- app/controllers/nasa_pictures_controller.rb
+++ app/controllers/nasa_pictures_controller.rb
@@ -1,7 +1,7 @@
 class NasaPicturesController < ApplicationController
   
   def index
-  	@nasa_info = NasaApodApi.get_nasa_info(params[:date])
+  	@nasa_info = NasaApodApi.get_nasa_info(params[:date] || Date.today)
   	@media_type_is_video = @nasa_info["media_type"].eql?("video")
   	@nasa_url = @nasa_info["url"]
   end
