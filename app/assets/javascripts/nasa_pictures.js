cd108b16b54fa1d490db919f872273f59ee3958d
Mary Katherine McKenzie <mkmckenzie91@gmail.com>
2016-04-10 17:21:56 -0400

    fix jquery show/hide info properly

--- app/assets/javascripts/nasa_pictures.js
+++ app/assets/javascripts/nasa_pictures.js
@@ -2,6 +2,13 @@
 // All this logic will automatically be available in application.js.
 $(document).ready(function() {
 	$("div#clickToOpen").on("click", function() {
-		$("div#summary").toggle("slow");
+		$("div#summary").fadeOut(800, function () {
+				$("div#showInfo").fadeIn(500);
+			});
+		});
+$("div#showInfo").on("click", function() {
+		$("div#showInfo").fadeOut(800, function () {
+			$("div#summary").fadeIn(800);
+		});
 	});
-});    
+}); 
